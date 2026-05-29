46137b46247f271df51986f009dc23c03c986cf9
TorrenceB <torrenceb90@gmail.com>
2026-05-29 11:49:10 -0600

    Update explanation copy

--- app/page.tsx
+++ app/page.tsx
@@ -45,14 +45,14 @@ export default function Home() {
 								<p className="text-sm">
 									A Burndown Chart tracks the teams progress over the cycle, showing two
 									lines: <b>Ideal</b> and <b>Actual</b>. The Ideal line is a negative
-									slope that begins at the total estimated effort(e.g 20) on day one and
-									ends at 0 on the final day of the cycle. The Actual line is the real
-									remaining effort and shifts up or down as project scope changes or
+									slope that begins at the total estimated effort (TEE) on the first day
+									of the cycle and ends at 0 TEE on the final day. The Actual line is the
+									real remaining effort and shifts up or down as project scope changes or
 									issues are completed.
 								</p>
 								<p className="text-sm">
-									The x-axis charts the time, and the y-axis is the estimated units
-									(“Story Points”, Hours, etc.).
+									The x-axis charts time, and the y-axis is the estimated units (“Story
+									Points”, Hours, etc.).
 								</p>
 							</div>
 							<div className="flex flex-col gap-2 w-full p-4 bg-[#161618] rounded-md">
@@ -83,13 +83,22 @@ export default function Home() {
 									line drops, moving closer to the Ideal line. In a real-world software
 									project, the goal is to keep Actual as close to Ideal as possible.
 								</p>
+								<p className="text-sm">
+									<a
+										href="https://github.com/TorrenceB/linear-integration"
+										target="_blank"
+										className="text-blue-500"
+									>
+										Check out the Github Repo.
+									</a>
+								</p>
 							</div>
 						</div>
 					</div>
 					{isLoading ? (
 						<p className="font-bold mt-40">Retrieving Active Cycle...</p>
 					) : (
-						burndown
+						<>{burndown}</>
 					)}
 				</>
 			</main>
